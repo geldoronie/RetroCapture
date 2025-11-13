10ea32a180a214a8389cc250fff9a11777508c10
Geldo Ronie <geldoronie@gmail.com>
2025-11-13 01:27:38 -0300

    Limpar arquivos de debug e testes não utilizados
    - Remover arquivos de debug (debug_*.glsl)
    - Remover scripts de teste (test_*.sh)
    - Remover diretório RetroArch não utilizado
    - Atualizar imgui.ini

--- test_swizzle.glsl
+++
@@ -1,7 +0,0 @@
-#version 330
-void main() {
-    vec3 test1 = 0.0.xxx;
-    vec3 test2 = mix(1.0.xxx + vec3(1.0), 1.0.xxx, 0.5);
-    vec3 test3 = pow(vec3(1.0), 0.70.xxx - 0.325);
-    gl_FragColor = vec4(test1 + test2 + test3, 1.0);
-}
